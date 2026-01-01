47a3d388b305fc2b0b99ae975c44a2e237aa124a
Yash Malviya <malviyarajyash27@gmail.com>
2026-01-01 18:45:57 +0530

    .

---
+++ Day1/class4.py
@@ -0,0 +1,27 @@
+#operrators in python
+a=10
+b=20
+print(a+b) #addition
+print(a-b) #subtraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a%b) #modulus
+print(a**b) #exponentiation
+
+#thi ia ncrement and decrement
+a+=5 #a=a+5
+a+=10 #a=a+10
+print(a)
+
+
+print("hello\n"*3) #string multiplication
+print("hello"+"world") #string concatenation
+print("hello" in "hello world") #membership operator
+print("hello" not in "hello world") #membership operator
+#comparison operators
+print(a==b) #equal to
+print(a!=b) #not equal to
+print(a>b) #greater than
+print(a<b) #less than
+print(a>=b) #greater than or equal to
+print(a<=b) #less than or equal to  
