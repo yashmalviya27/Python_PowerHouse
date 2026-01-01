657079618170766a47c13ad1d958e77f034bd3e8
Yash Malviya <malviyarajyash27@gmail.com>
2026-01-01 12:01:14 +0530

    this is the day 1 second commit

---
+++ Day1/comment.py
@@ -0,0 +1,40 @@
+#this is the first comment this is single line comment
+
+''' this is a 
+multi line comment
+this is the second line of comment  '''
+
+
+#now jump into variables
+
+var1=5  #this is variable declaration
+var2=10 #this is another variable declaration   
+print(var1+var2)  #this will print the sum of two variables
+
+'''the variable are used to store data in a program
+we can use these variables later in the program'''
+
+# the naming conventions for variables are as follows:
+# 1. camelCase
+yashMalviya = 10
+# 2. snake_case
+yash_malviya = 10
+# 3. PascalCase 
+YashMalviya = 10
+# 4. snakeCase
+yash_malviya = 10
+
+
+#daratypes in python
+var_int = 10          # integer datatype
+var_float = 10.5     # float datatype
+complex_num = 3 + 5j  # complex datatype
+var_str = "hello"    # string datatype
+var_bool = True      # boolean datatype
+
+print(type(var_int))      # prints <class 'int'>
+print(type(var_float))    # prints <class 'float'>
+print(type(complex_num))  # prints <class 'complex'>
+print(type(var_str))      # prints <class 'str'>
+print(type(var_bool))     # prints <class 'bool'>
+#type() function is used to check the datatype of a variable
---
+++ Day1/string.py
@@ -0,0 +1,16 @@
+'''now we are in string.py file'''
+str1 = "Hello, World!"  # This is a string variable
+str2 = 'Python is fun.'  # Another string variable
+print(str1)  # Output: Hello, World!
+print(str2)  # Output: Python is fun.
+
+
+#string indexing and slicing
+sample_str = "PowerHouse"
+print(sample_str[0])  # Output: P
+print(sample_str[1])  # Output: o
+print(sample_str[-1])  # Output: e
+print(sample_str[2:5])  # Output: wer
+print(sample_str[:5])  # Output: Power
+print(sample_str[5:])  # Output: House
+
