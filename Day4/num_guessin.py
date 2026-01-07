a2a98a26267b895c29ea635434da8e89d22c4349
Yash Malviya <malviyarajyash27@gmail.com>
2026-01-07 14:24:04 +0530

    :)

---
+++ Day4/num_guessin.py
@@ -0,0 +1,22 @@
+# Game.1: Number Guessing Game
+""" import random
+
+number_to_guess = random.randint(1, 100)
+attempts = 0
+
+while True:
+    guessed = int(input("Guess the number between 1 and 100: "))
+    attempts += 1
+
+    if guessed == number_to_guess:
+        print(f"Congratulations! You guessed the number in {attempts} attempts.")
+        break
+    elif guessed < number_to_guess:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
+
+print("Thanks for playing!") """
+
+# Game.2: Stone Paper Scissors Game
+
