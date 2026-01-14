e7cca18c7eeff2afed2e952205325782dfa44480
Yash Malviya <malviyarajyash27@gmail.com>
2026-01-14 22:46:54 +0530

    :)

--- Day9/class17.py
+++ Day9/class17.py
@@ -2,7 +2,7 @@
 
 # Question.1: leetcode 1. Two Sum
 
- class Solution:
+""" class Solution:
     def twoSum(self, nums:list , target:int) -> list[int]:
         d = {}
         for i in range(len(nums)):
@@ -13,20 +13,20 @@
                 d[nums[i]] = i
 
 s = Solution()
-print(s.twoSum(nums=[2,7,11,15], target=13)) 
+print(s.twoSum(nums=[2,7,11,15], target=13)) """
 
 # Question.2: leetcode 217. Contains Duplicate
 
 class Solution:
     def containsDuplicate(self , nums: list[int]) -> bool:
         return len(set(nums)) != len(nums)
-        d = {}
-        for i in nums:
-            if i in d:
-                return True
-            else:
-                d[i]=1
-        return False
+        # d = {}
+        # for i in nums:
+        #     if i in d:
+        #         return True
+        #     else:
+        #         d[i]=1
+        # return False
     
 s = Solution()
 print(s.containsDuplicate(nums=[2,3,1])) 
