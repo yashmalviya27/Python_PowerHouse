daea79b2fff559a0d17d9dff449b6cac32b798c5
Yash Malviya <malviyarajyash27@gmail.com>
2026-01-15 22:50:01 +0530

    :

--- Day10/smallPro.py
+++ Day10/smallPro.py
@@ -1,3 +1,4 @@
+# in this i create a class and create 2 object of it
 class student:
     def __init__(self, name, age, marks):
         self.name = name
@@ -6,11 +7,11 @@ class student:
     
     def display(self):
         print(f"Name: {self.name}\nAge: {self.age}\nMarks: {self.marks}")
-
+# create 1 object of class 
 s1 = student("Yash", 20, 80)
 s1.display()
 
 print("-----------------")
-
+# create 2 object of class
 s2 = student("Raj", 30, 80)
 s2.display()
