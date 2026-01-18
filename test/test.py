de1e207e789846bd3db38aba3983ae992540afe5
Yash Malviya <malviyarajyash27@gmail.com>
2026-01-18 22:14:49 +0530

    :

---
+++ test/test.py
@@ -0,0 +1,12 @@
+>>> class Dog:
+...     species = "Canis familiaris"
+...     def __init__(self, name, age):
+...         self.name = name
+...         self.age = age
+...
+>>> d = Dog("Fido", 3)
+>>> d.species
+'Canis familiaris'
+>>> d.name
+'Fido'
+>>> d.age           
